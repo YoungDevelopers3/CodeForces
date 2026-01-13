d96ab7dfdaa021b8b77dc792c3d0654e67b4b0eb
YoungDevelopers3 <deepakbhatti889@gmail.com>
2026-01-13 18:27:49 -0500

    solved

--- demo.cpp
+++ demo.cpp
@@ -1,37 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    int t;
-    cin >> t; // number of test cases
 
-    while (t--)
-    {
-        int n;
-        cin >> n;
-
-        vector<int> a(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-
-        long long sum = 0;
-        int mn = a[0], mx = a[0];
-
-        for (int x : a)
-        {
-            sum += x;
-            mn = min(mn, x);
-            mx = max(mx, x);
-        }
-
-        cout << "Sum = " << sum << "\n";
-        cout << "Min = " << mn << "\n";
-        cout << "Max = " << mx << "\n";
-        cout << "----\n";
-    }
-
-    return 0;
+    cout << 'b' - 'a';
 }
